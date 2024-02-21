fea591a522eeebac95f33264ce574a116ae5b8fa
bhargavdevelopr <8374722496p@gmail.com>
2024-02-21 07:42:44 +0000

    All the test cases passed but the output of the project was not as desired so please tell me where the problem is

---
+++ src/components/About/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      className="image"
+      alt="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+    />
+    <h1 className="about-heading">About</h1>
+    <p className="about-para">I love to create! Iam a Front-end-developer</p>
+  </div>
+)
+
+export default About
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+// Write your JS code here
+
+import './index.css'
+
+const NotFound = () => (
+  <div className="error-container">
+    <img
+      className="error-image"
+      alt="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png "
+    />
+    <h1 className="error-heading">Not Found</h1>
+  </div>
+)
+
+export default NotFound
